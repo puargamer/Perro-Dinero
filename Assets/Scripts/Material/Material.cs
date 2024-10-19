16332801c20f61cb423ed44eae7392552305676b
laerfugi <rquiambao@mail.sfsu.edu>
2024-10-19 00:06:51 -0700

    fixed material color

--- Assets/Scripts/Material/Material.cs
+++ Assets/Scripts/Material/Material.cs
@@ -7,10 +7,37 @@ public class Material : MonoBehaviour
     public MaterialType materialType; // set an enum
     [SerializeField] private MaterialSpawner spawner;
 
+    private Renderer materialRenderer;
+
     public void Setup(MaterialSpawner spawner, MaterialType type)
     {
         this.spawner = spawner;
         materialType = type;
+        materialRenderer = GetComponent<Renderer>();
+        SetMaterialColor();
+    }
+
+    private void SetMaterialColor()
+    {
+        Color wantedColor;
+
+        switch (materialType)
+        {
+            case MaterialType.Red:
+                wantedColor = Color.red;
+                break;
+            case MaterialType.Blue:
+                wantedColor = Color.blue;
+                break;
+            case MaterialType.Yellow:
+                wantedColor = Color.yellow;
+                break;
+            default:
+                wantedColor = Color.white;
+                break;
+        }
+
+        materialRenderer.material.color = wantedColor;
     }
 
     void OnTriggerEnter(Collider other)
