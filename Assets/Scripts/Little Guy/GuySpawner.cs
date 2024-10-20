cd3b95c1378be7074585b47890fa18b676220835
laerfugi <rquiambao@mail.sfsu.edu>
2024-10-19 23:10:51 -0700

    created tutorial and first level scene

---
+++ Assets/Scripts/Little Guy/GuySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuySpawner : MonoBehaviour
+{
+    public LittleGuyFactory littleGuyFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            littleGuyFactory.CreateLittleGuy(new Vector3(0f, 1.5f, 0f), (MaterialType)0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Little Guy/LittleGuyFactory.cs
+++ Assets/Scripts/Little Guy/LittleGuyFactory.cs
@@ -9,15 +9,13 @@ public class LittleGuyFactory : MonoBehaviour
     public GameObject CreateLittleGuy(Vector3 position, MaterialType materialType)
     {
         GameObject littleGuy = Instantiate(littleGuyPrefab, position, Quaternion.identity);
-        littleGuy.GetComponent<NavMeshAgent>().enabled = false;
-        StartCoroutine(NVMAgentActivate(littleGuy, materialType));
+        StartCoroutine(InstantiateLittleGuy(littleGuy, materialType));
         return littleGuy;
     }
 
-    private IEnumerator NVMAgentActivate(GameObject littleGuy, MaterialType materialType)
+    private IEnumerator InstantiateLittleGuy(GameObject littleGuy, MaterialType materialType)
     {
         yield return new WaitForEndOfFrame();
-        littleGuy.GetComponent<NavMeshAgent>().enabled = true;
         LittleGuyNav littleGuyNav = littleGuy.GetComponent<LittleGuyNav>();
         littleGuyNav.Setup(materialType);
     }
