b1551b6ac415ad1608ceee650d9d04634df5678e
puargamer <79729942+puargamer@users.noreply.github.com>
2024-11-16 22:14:04 -0800

    update dialogue box
    put dialogue box within ui, added skip functionality

--- Assets/Scripts/nachos testing/Dialogue.cs
+++ Assets/Scripts/nachos testing/Dialogue.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Dialogue : MonoBehaviour
 {
     public GameObject textbox;
+    public GameObject cursor;
     public TMP_Text text;
     public string[] lines;
     public float textSpeed;
     private int index;
+
+    private Coroutine _TypeLine;    //reference to text animation coroutine, used to skip animation
 
     public DialogueSounds dialogueSounds;   //sound
 
@@ -25,6 +28,7 @@ public class Dialogue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         //squish
         originalyScale = character.transform.localScale.y;
         originalyPos = character.transform.position.y;
@@ -35,9 +39,21 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //continue to next line
         if (text.text == lines[index])
         {
             nextLine();
+        }
+
+        //skip text animation
+        else if (text.text != lines[index])
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Debug.Log("cuh");
+                StopCoroutine(_TypeLine);
+                text.text = lines[index];
+            }
         }
     }
 
@@ -47,69 +63,74 @@ public class Dialogue : MonoBehaviour
         StopAllCoroutines();
         textbox.SetActive(true);
         index = 0;
-        StartCoroutine(TypeLine());
+        _TypeLine = StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
-        foreach (char  c in lines[index].ToCharArray()) 
+        cursor.SetActive(false);
+
+        foreach (char c in lines[index].ToCharArray()) 
         {
+
+            //normal text progression
             text.text += c;
             dialogueSounds.PlayCharSound(c);    //play sound
 
-            //squish
-            float tempYScale = 0;
-            float tempYPos = 0;
-
-            if (flip)
-            {
-                tempYScale = character.transform.localScale.y - scaleDiff;
-                tempYPos = character.transform.position.y - posDiff;
-            }
-            else
-            {
-                tempYScale = character.transform.localScale.y + scaleDiff;
-                tempYPos = character.transform.position.y + posDiff;
-            }
-            flip = !flip;
-
-            character.transform.localScale = new Vector3(character.transform.localScale.x, tempYScale, character.transform.localScale.z);
-            character.transform.position = new Vector3(character.transform.position.x, tempYPos, character.transform.position.z);
-
+            squishAnimation();
 
             yield return new WaitForSeconds(textSpeed);
         }
     }
 
-    IEnumerator EndDialogue()
+    void squishAnimation()
+    {
+        float tempYScale = 0;
+        float tempYPos = 0;
+
+        if (flip)
+        {
+            tempYScale = character.transform.localScale.y - scaleDiff;
+            tempYPos = character.transform.position.y - posDiff;
+        }
+        else
+        {
+            tempYScale = character.transform.localScale.y + scaleDiff;
+            tempYPos = character.transform.position.y + posDiff;
+        }
+        flip = !flip;
+
+        character.transform.localScale = new Vector3(character.transform.localScale.x, tempYScale, character.transform.localScale.z);
+        character.transform.position = new Vector3(character.transform.position.x, tempYPos, character.transform.position.z);
+    }
+
+    void EndDialogue()
     {
         //reset squish
         character.transform.localScale = new Vector3(character.transform.localScale.x, originalyScale, character.transform.localScale.z);
         character.transform.position = new Vector3(character.transform.position.x, originalyPos, character.transform.position.z);
 
-        yield return new WaitForSeconds(5);
         textbox.SetActive(false);
     }
 
     void nextLine()
     {
-        if (index < lines.Length - 1)
-        {
-            index++;
-            //text.text = "";
-            //StartCoroutine (TypeLine());
-            StartCoroutine(NextLineWait());
-        }
-        else
-        {
-            StartCoroutine(EndDialogue());
-        }
-    }
+        cursor.SetActive(true);
 
-    IEnumerator NextLineWait()
-    {
-        yield return new WaitForSeconds(2);
-        text.text = "";
-        StartCoroutine(TypeLine());
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            //display next dialogue line
+            if (index < lines.Length - 1)
+            {
+                index++;
+
+                text.text = "";
+                _TypeLine = StartCoroutine(TypeLine());
+            }
+
+            //if no more dialogue, end
+            else {EndDialogue();}
+        }
     }
 }
