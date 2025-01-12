df4ae18630a3c28f3f9a38a0f0acd1250007c08f
puargamer <79729942+puargamer@users.noreply.github.com>
2025-01-12 15:00:42 -0800

    fixed RecipeBook prefab bug

---
+++ Assets/Scripts/Interactable/Item/Fish_ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Fish_ItemData : ItemData
+{
+    public CombinationType combinationType;
+    public int price;
+}
