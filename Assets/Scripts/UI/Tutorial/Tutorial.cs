1c6df8c1a20fe5cbd672467586a80adca6546ce6
laerfugi <rquiambao@mail.sfsu.edu>
2025-01-13 21:13:03 -0800

    changed sprites in main scene, added tutorial UI

--- Assets/Scripts/UI/Tutorial/Tutorial.cs
+++ Assets/Scripts/UI/Tutorial/Tutorial.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    public GameObject dialogueText;
+    public GameObject[] popUps;
+    private int popUpIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        popUpIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
     }
+
+    public void NextPage() { popUpIndex++; }
+    public void PreviousPage() { popUpIndex--; }
+    public void CloseTutUI() { gameObject.SetActive(false); }
+
 }
